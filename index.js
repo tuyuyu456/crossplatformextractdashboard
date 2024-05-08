99d7c8354e8802556a08e125837fbde5c5e0a527
tuyuyu456 <maccadee90nam1311@outlook.com>
2024-05-08 19:04:50 +0700

    start

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function singleNumber(nums) {
-  let result = 0;
-  for (const num of nums) {
-    result ^= num;
+function findOrder(numCourses, prerequisites) {
+  const graph = new Array(numCourses).fill(0).map(() => []);
+  const inDegree = new Array(numCourses).fill(0);
+  for (const [course, pre] of prerequisites) {
+    graph[pre].push(course);
+    inDegree[course]++;
   }
-  return result;
+  const queue = [];
+  for (let i = 0; i < numCourses; i++) {
+    if (inDegree[i] === 0) queue.push(i);
+  }
+  const result = [];
+  while (queue.length) {
+    const course = queue.shift();
+    result.push(course);
+    for (const nextCourse of graph[course]) {
+      inDegree[nextCourse]--;
+      if (inDegree[nextCourse] === 0) queue.push(nextCourse);
+    }
+  }
+  return result.length === numCourses ? result : [];
 }
