be26663dbcd44a7a0ab0410f4ed0420ebe937221
tuyuyu456 <maccadee90nam1311@outlook.com>
2024-05-15 10:11:59 +0700

    aptos

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-function generate(numRows) {
-  const triangle = [];
-  for (let i = 0; i < numRows; i++) {
-    const row = new Array(i + 1).fill(1);
-    for (let j = 1; j < row.length - 1; j++) {
-      row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
-    }
-    triangle.push(row);
-  }
-  return triangle;
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
